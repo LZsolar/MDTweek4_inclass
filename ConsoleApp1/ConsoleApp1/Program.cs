30cdcdcf30400266290c7daa6361b2a54771a462
LZsolar <minecraftradioevil@gmail.com>
2022-09-09 09:40:55 +0700

    FirstTime setting

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,11 +6,7 @@ namespace MDTwork1023
     {
         static void Main(string[] args)
         {
-            string name = InputName();
-            string score = InputScore();
-            string grade = GetGrade(score);
-
-            PrintGrade(name, score, grade);
+            
         }
     }
 }
