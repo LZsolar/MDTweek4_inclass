1dd077b47e9ae5d455077a5ca551c62698184fcc
LZsolar <minecraftradioevil@gmail.com>
2022-09-09 09:35:06 +0700

    FirstTimePush

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MDTwork1023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = InputName();
+            string score = InputScore();
+            string grade = GetGrade(score);
+
+            PrintGrade(name, score, grade);
+        }
+    }
+}
